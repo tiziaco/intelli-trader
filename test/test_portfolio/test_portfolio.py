ffcd7e1ec4e2b2407800fa651d8f43eb7f16831e
tiziaco <tiziano.iaco@gmail.com>
2024-05-26 11:13:38 +0200

    add tests process transactions

--- test/test_portfolio/test_portfolio.py
+++ test/test_portfolio/test_portfolio.py
@@ -3,6 +3,7 @@ from datetime import datetime
 
 from itrader.portfolio_handler.portfolio import Portfolio
 from itrader.portfolio_handler.transaction import Transaction, TransactionType
+from itrader import idgen
 
 
 class TestPortfolio(unittest.TestCase):
@@ -37,12 +38,12 @@ class TestPortfolio(unittest.TestCase):
 		"""
 		# Buy 1 of BTC over one transactions
 		buy_txn = Transaction(datetime.now(), TransactionType.BUY,
-							'BTCUSDT', 40000, 1, 0, None)
+							'BTCUSDT', 40000, 1, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(buy_txn)
 
 		# Sell 1 of BTC over one transactions
 		sell_txn = Transaction(datetime.now(), TransactionType.SELL,
-							'BTCUSDT', 42000, 1, 0, None)
+							'BTCUSDT', 42000, 1, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(sell_txn)
 
 
@@ -62,14 +63,13 @@ class TestPortfolio(unittest.TestCase):
 		"""
 		# Sell 1 of BTC over one transactions
 		sell_txn = Transaction(datetime.now(), TransactionType.SELL,
-							'BTCUSDT', 42000, 1, 0, None)
+							'BTCUSDT', 42000, 1, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(sell_txn)
 
 		# Buy 1 of BTC over one transactions
 		buy_txn = Transaction(datetime.now(), TransactionType.BUY,
-							'BTCUSDT', 40000, 1, 0, None)
+							'BTCUSDT', 40000, 1, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(buy_txn)
-
 
 		# Assert the result after processing the transactions
 		self.assertEqual(len(self.portfolio.positions), 0)
@@ -80,16 +80,16 @@ class TestPortfolio(unittest.TestCase):
 		self.assertEqual(self.portfolio.total_unrealised_pnl, 0)
 	
 	def test_multiple_buys_followed_by_sell(self):
-		# Buy 2 units of AAPL at $150
-		buy_txn1 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 38000, 2, 0, None)
+		# Buy 2 units of BTC at $38000
+		buy_txn1 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 38000, 2, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(buy_txn1)
 
 		# Buy 1 unit of BTC at $40000
-		buy_txn2 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 40000, 1, 0, None)
+		buy_txn2 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 40000, 1, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(buy_txn2)
 
-		# Sell 1 unit of BTC at $42000
-		sell_txn = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 45000, 3, 0, None)
+		# Sell 1 unit of BTC at $45000
+		sell_txn = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 45000, 3, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(sell_txn)
 
 		# Assert the result after processing the transactions
@@ -98,19 +98,19 @@ class TestPortfolio(unittest.TestCase):
 		self.assertEqual(self.portfolio.cash, 169000)  # Cash after transactions
 		self.assertEqual(self.portfolio.total_equity, 169000)  # Total equity after transactions
 		self.assertEqual(self.portfolio.total_unrealised_pnl, 0)  # Total unrealized P&L
-		self.assertEqual(self.portfolio.total_realised_pnl, 0)  # Total realized P&L
+		self.assertAlmostEqual(self.portfolio.total_realised_pnl, 19000, 2)  # Total realized P&L
 
 	def test_sell_followed_by_multiple_buys(self):
-		# Sell 1 unit of BTC at $45000
-		sell_txn = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 45000, 3, 0, None)
+		# Sell 3 unit of BTC at $45000
+		sell_txn = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 45000, 3, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(sell_txn)
 
 		# Buy 1 units of BTC at $40000
-		buy_txn1 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 40000, 1, 0, None)
+		buy_txn1 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 40000, 1, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(buy_txn1)
 
 		# Buy 2 unit of BTC at $38000
-		buy_txn2 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 38000, 2, 0, None)
+		buy_txn2 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 38000, 2, 0, None, idgen.generate_transaction_id())
 		self.portfolio.process_transaction(buy_txn2)
 
 		# Assert the result after processing the transactions
@@ -119,7 +119,84 @@ class TestPortfolio(unittest.TestCase):
 		self.assertEqual(self.portfolio.cash, 169000)  # Cash after transactions
 		self.assertEqual(self.portfolio.total_equity, 169000)  # Total equity after transactions
 		self.assertEqual(self.portfolio.total_unrealised_pnl, 0)  # Total unrealized P&L
-		self.assertEqual(self.portfolio.total_realised_pnl, 0)  # Total realized P&L
+		self.assertAlmostEqual(self.portfolio.total_realised_pnl, 19000, 2)  # Total realized P&L
+
+	def test_transaction_with_commission(self):
+
+		# Buy 2 units of BTC at $38000 with $100 commission
+		buy_txn1 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 38000, 2, 100, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(buy_txn1)
+
+		# Sell 2 units of BTC at $40000 with $100 commission
+		sell_txn = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 40000, 2, 100, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(sell_txn)
+
+		# Assert the result after processing the transactions
+		self.assertEqual(len(self.portfolio.positions), 0)  # No position remaining
+		self.assertEqual(len(self.portfolio.closed_positions), 1)  # One position (BTCUSDT) closed
+		self.assertEqual(self.portfolio.cash, 154000 - 200)  # Cash after transactions considering commissions
+		self.assertEqual(self.portfolio.total_realised_pnl, 4000 - 200)  # Realized P&L after commissions
+
+	def test_partial_closure(self):
+	
+		# Buy 5 units of BTC at $40000
+		buy_txn = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 40000, 5, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(buy_txn)
+
+		# Sell 3 units of BTC at $45000
+		sell_txn = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 45000, 3, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(sell_txn)
+
+		# Assert the result after processing the transactions
+		self.assertEqual(len(self.portfolio.positions), 1)  # One position remaining
+		self.assertEqual(self.portfolio.positions['BTCUSDT'].net_quantity, 2)  # 2 units remaining
+		self.assertEqual(self.portfolio.cash, 150000 - (40000 * 5) + (45000 * 3))  # Cash after transactions
+		self.assertEqual(self.portfolio.total_realised_pnl, 15000)  # Realized P&L for the closed portion
+
+	def test_multiple_assets(self):
+    
+		# Buy 1 unit of BTC at $40000
+		buy_btc = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 40000, 1, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(buy_btc)
+
+		# Buy 2 units of ETH at $2500
+		buy_eth = Transaction(datetime.now(), TransactionType.BUY, 'ETHUSDT', 2500, 2, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(buy_eth)
+
+		# Sell 1 unit of BTC at $42000
+		sell_btc = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 42000, 1, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(sell_btc)
+
+		# Assert the result after processing the transactions
+		self.assertEqual(len(self.portfolio.positions), 1)  # One position remaining (ETH)
+		self.assertEqual(len(self.portfolio.closed_positions), 1)  # One position (BTC) closed
+		self.assertEqual(self.portfolio.positions['ETHUSDT'].net_quantity, 2)  # 2 units of ETH remaining
+		self.assertEqual(self.portfolio.cash, 150000 - 40000 + 42000 - 2500 * 2)  # Cash after transactions
+		self.assertEqual(self.portfolio.total_realised_pnl, 2000)  # Realized P&L for BTC
+
+	def test_mixed_buy_sell_transactions(self):
+		
+		# Buy 2 units of BTC at $38000
+		buy_txn1 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 38000, 2, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(buy_txn1)
+
+		# Sell 1 unit of BTC at $40000
+		sell_txn1 = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 40000, 1, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(sell_txn1)
+
+		# Buy 1 unit of BTC at $37000
+		buy_txn2 = Transaction(datetime.now(), TransactionType.BUY, 'BTCUSDT', 37000, 1, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(buy_txn2)
+
+		# Sell 2 units of BTC at $39000
+		sell_txn2 = Transaction(datetime.now(), TransactionType.SELL, 'BTCUSDT', 39000, 2, 0, None, idgen.generate_transaction_id())
+		self.portfolio.process_transaction(sell_txn2)
+
+		# Assert the result after processing the transactions
+		self.assertEqual(len(self.portfolio.positions), 0)  # No position remaining
+		self.assertEqual(len(self.portfolio.closed_positions), 1)  # One position (BTCUSDT) closed
+		self.assertEqual(self.portfolio.cash, 155000)  # Cash after transactions
+		self.assertAlmostEqual(self.portfolio.total_realised_pnl, 5000, 2)  # Realized P&L
 
 
 if __name__ == "__main__":
